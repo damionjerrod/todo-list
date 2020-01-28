f8ca6ca5bc82c29f38a1e6a4512c0e443e9bd147
damionjerrod <damion.dawson@gmail.com>
2020-01-28 09:07:33 -0500

    somewhat finished modal

--- todo-list-starter-files/src/components/InputForm.jsx
+++ todo-list-starter-files/src/components/InputForm.jsx
@@ -1,5 +1,27 @@
 import React from "react";
-import "../styles/Modal.css";
+// import "../styles/Modal.css";
+
+const backdropStyle = {
+    position: "fixed",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "rgba(0,0,0,0.6)",
+    padding: 50
+}
+
+const modalStyle = {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    maxWidth: 600,
+    minHeight: 200,
+    margin: "0 auto",
+    marginTop: 200,
+    marginBottom: 100,
+    padding: 50,
+    position: "relative"
+}
 
 const InputForm = props => {
     // const showHide = props.show ? "modal display-block" : "modal display-none";    
@@ -7,22 +29,23 @@ const InputForm = props => {
         return null;
     }
     return (
-        <div className="modal-content">
-            <form 
-                onSubmit={props.newItemSubmitHandler}
-                className="todoInput">
-                <input
-                    className="input"
-                    type="text"
-                    onChange={props.handleItemInput}
-                    value={props.pendingItem}
-                    placeholder="Add an item"
-                    />
-                <button type="submit" name="submit" value="submit">
-                    add
-                </button>
-                <button type="button" onClick={props.handleClose}>Close</button>
-            </form>
+        <div className="modal" style={backdropStyle}>
+            <div className="modal-content" style={modalStyle}>
+                <form 
+                    onSubmit={props.newItemSubmitHandler}
+                    >
+                    <input
+                        type="text"
+                        onChange={props.handleItemInput}
+                        value={props.pendingItem}
+                        placeholder="Add an item"
+                        />
+                    <button type="submit" name="submit" value="submit">
+                        add
+                    </button>
+                    <button type="button" className="close-btn" onClick={props.handleClose}>Close</button>
+                </form>
+            </div>
         </div>
     );
 };
